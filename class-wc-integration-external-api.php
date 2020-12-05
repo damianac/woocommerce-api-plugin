a0cc12bdeada0edb92b3fe51ed5ad1083c029af3
Luka <luka@mindfuel.dev>
2020-12-05 20:40:02 +0100

    feat: code content

--- class-wc-integration-external-api.php
+++ class-wc-integration-external-api.php
@@ -1,1 +1,43 @@
 <?php
+/**
+ * Integrates `Woocommerce External API` with WooCommerce
+ *
+ * @package   Woocommerce External Api Integration
+ * @category Integration
+ * @author   Luka Damjanac luka@exnihilo.dev
+ */
+if ( ! class_exists( 'WC_External_API_Integration' ) ) :
+  class WC_External_API_Integration extends WC_Integration {
+    /**
+     * Init and hook in the integration.
+     */
+    public function __construct() {
+      global $woocommerce;
+      $this->id                 = 'external-api-integration';
+      $this->method_title       = __( 'External API Integration');
+      $this->method_description = __( 'External API Integration plugin fetches data from external API and displays it');
+      // Load the settings.
+      $this->init_form_fields();
+      $this->init_settings();
+      // Define user set variables.
+      $this->external_api_key = $this->get_option( 'external_api_key' );
+      // Actions.
+      add_action( 'woocommerce_update_options_integration_' .  $this->id, array( $this, 'process_admin_options' ) );
+    }
+    /**
+     * Initialize integration settings form fields.
+     */
+    public function init_form_fields() {
+      $this->form_fields = array(
+          'external_api_key' => array(
+              'title'             => __( 'External API Key'),
+              'type'              => 'text',
+              'description'       => __( 'Enter your API key'),
+              'desc_tip'          => true,
+              'default'           => '',
+              'css'      => 'width: 270px;',
+          ),
+      );
+    }
+  }
+endif;
--- woocommerce-external-api.php
+++ woocommerce-external-api.php
@@ -1,1 +1,67 @@
 <?php
+
+/**
+ * Plugin Name: WooCommerce - External API
+ * Plugin URI: https://saucal.com
+ * Description: A plugin that fetches external API and displays it inside of WP Widget
+ * and in Woocommerce `My Account` tab
+ * Author: Luka Damjanac
+ * Author URI:  https://saucal.com
+ * Version: 1.0
+*/
+if ( ! class_exists( 'WC_External_API' ) ) :
+  class WC_External_API {
+    /**
+     * Construct the plugin.
+     */
+    public function __construct() {
+      add_action( 'plugins_loaded', array( $this, 'init' ) );
+    }
+    /**
+     * Initialize the plugin.
+     */
+    public function init() {
+      // Checks whether WooCommerce is installed.
+      if ( class_exists( 'WC_Integration' ) ) {
+        // Include our integration class.
+        include_once 'class-wc-integration-external-api.php';
+        // Register the integration.
+        add_filter('woocommerce_integrations', array( $this, 'add_integration' ));
+
+        // Set the plugin slug
+        define( 'MY_PLUGIN_SLUG', 'wc-settings' );
+
+        // Setting action for plugin
+        add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'WC_my_custom_plugin_action_links' );
+      }
+    }
+    /**
+     * Add a new integration to WooCommerce.
+     */
+    public function add_integration($integrations) {
+      $integrations[] = 'WC_External_API_Integration';
+      return $integrations;
+    }
+
+    public function fetch_api(
+        $elements = [],
+        $url = 'https://httpbin.org/post',
+        $request_method = 'POST'
+        )
+    {
+    
+    }
+
+  }
+
+  $WC_external_API = new WC_External_API(__FILE__);
+
+  function WC_my_custom_plugin_action_links( $links ) {
+
+    $links[] = '<a href="'. menu_page_url( MY_PLUGIN_SLUG, false ) .'&tab=integration">Settings</a>';
+    return $links;
+  }
+
+endif;
+
+?>
